871a62c2f633d38fe344d834969d9cf54cbf5268
giuseppegrilli <giugrilli@gmail.com>
2022-09-21 10:47:20 +0200

    fix tabBarUnderlineStyle

--- index.js
+++ index.js
@@ -381,7 +381,7 @@ const ScrollableTabView = createReactClass({
       tabBarProps.textStyle = this.props.tabBarTextStyle;
     }
     if (this.props.tabBarUnderlineStyle) {
-      tabBarProps.underlineStyle = this.props.tabBarUnderlineStyle;
+      tabBarProps.tabBarUnderlineStyle = this.props.tabBarUnderlineStyle;
     }
     if (overlayTabs) {
       tabBarProps.style = {
